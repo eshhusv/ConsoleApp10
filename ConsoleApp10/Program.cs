076d7ee707ac9b22178c61ac006e825c27652952
Артём Коваленко <ak7527203@gmail.com>
2022-11-26 17:27:39 +0200

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите сообщение: ");
+string text = Console.ReadLine();
+char ch = '*';
+int indexOfChar = text.IndexOf(ch);
+Console.WriteLine($"Смивол '{ch}' первый раз встречается на {indexOfChar + 1} позиции");
